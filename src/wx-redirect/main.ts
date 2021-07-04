9cb1364df9a3e538dada1ca91f93d9080fd77834
zhuxian <18270244870@163.com>
2021-07-04 16:11:21 +0800

    update redirect

--- src/wx-redirect/main.ts
+++ src/wx-redirect/main.ts
@@ -18,9 +18,9 @@ const base_url = process.env.BASE_WECHAT_REDIRECT;
 if (code) {
   // window.location.href = `${URL}front/test/#/?code=${code}&url=${encodeURIComponent(state)}`;
   if (state === "/?")
-    window.location.href = `${base_url}/#/?code=${code}&url=${state}`;
-  else window.location.href = `${base_url}/#${state}&code=${code}`;
+    window.location.replace(`${base_url}/#/?code=${code}&url=${state}`);
+  else window.location.replace(`${base_url}/#${state}&code=${code}`);
 } else {
   const url = encodeURIComponent(`${base_url}/redirect.html`);
-  window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0074a25ead29c1d&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=${state}&connect_redirect=1#wechat_redirect`;
+  window.location.replace(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0074a25ead29c1d&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=${state}&connect_redirect=1#wechat_redirect`);
 }
