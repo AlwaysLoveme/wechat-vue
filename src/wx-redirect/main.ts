3517aa6f05afa43b1dbf6064a1a60706506f8923
zhuxian <18270244870@163.com>
2021-07-06 10:04:17 +0800

    fixed wechat redirect

--- src/wx-redirect/main.ts
+++ src/wx-redirect/main.ts
@@ -15,11 +15,16 @@ const code = getUrlParam("code");
 const state = getUrlParam("state");
 const base_url = window.location.origin;
 if (code) {
-  const url = decodeURIComponent(state)
   // window.location.href = `${URL}front/test/#/?code=${code}&url=${encodeURIComponent(state)}`;
   if (state === "/?")
     window.location.replace(`${base_url}/#/?code=${code}&url=${state}`);
-  else window.location.replace(`${base_url}/#${url}&code=${code}`);
+  else {
+    const url = decodeURIComponent(state as string);
+    const redirectUrl = `${base_url}/#${url}${
+      url.indexOf("?") !== -1 ? "&" : "?"
+    }code=${code}`;
+    window.location.replace(redirectUrl);
+  }
 } else {
   const url = encodeURIComponent(`${base_url}/redirect.html`);
   window.location.replace(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0074a25ead29c1d&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=${state}&connect_redirect=1#wechat_redirect`);
